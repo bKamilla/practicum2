0ddfe07e186dc66037ef45e8bb63eacf22710bc7
bKamilla <k.batrakova@g.nsu.ru>
2024-03-01 19:50:06 +0700

    2

--- 6.py
+++ 6.py
@@ -1,1 +1,8 @@
 #practicum2
+
+weightf = int(input("Введите вес в фунтах:"))
+heightd = int(input("Введите высоту в дюймах:"))
+heightm = 0.0254 * heightd
+weightkg = weightf * 0.454
+IMT = weightkg / (heightm * heightm)
+print(round(IMT, 2))
