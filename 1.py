bdbfa9fafcc14ba7cd3ea9f5f3ea9dce2c3abfe4
bKamilla <k.batrakova@g.nsu.ru>
2024-03-01 18:45:13 +0700

    3

--- 1.py
+++ 1.py
@@ -1,1 +1,10 @@
 #practicum2
+
+summa = int(input())
+silverq = 96
+goldenq = 96 / 16
+silverprice = 48
+silversum = 48 * silverq
+goldensum = summa - silversum
+goldenprice = goldensum / goldenq
+print(round(goldenprice))
